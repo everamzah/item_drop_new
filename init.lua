32680caf480de11c52da2ca1830fe5d9272b9a5b
James Stevenson <everamzah@gmail.com>
2016-07-02 12:10:43 -0400

    Style, roughly.

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 minetest.register_globalstep(function(dtime)
-	for _,player in ipairs(minetest.get_connected_players()) do
+	for _, player in ipairs(minetest.get_connected_players()) do
 		if player:get_hp() > 0 or not minetest.setting_getbool("enable_damage") then
 			local pos = player:getpos()
-			pos.y = pos.y+0.5
+			pos.y = pos.y + 0.5
 			local inv = player:get_inventory()
 			
-			for _,object in ipairs(minetest.get_objects_inside_radius(pos, 1)) do
+			for _, object in ipairs(minetest.get_objects_inside_radius(pos, 1)) do
 				if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
 					if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
 						inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
@@ -21,17 +21,17 @@ minetest.register_globalstep(function(dtime)
 				end
 			end
 			
-			for _,object in ipairs(minetest.get_objects_inside_radius(pos, 2)) do
+			for _, object in ipairs(minetest.get_objects_inside_radius(pos, 2)) do
 				if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
 					if object:get_luaentity().collect then
 						if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
 							local pos1 = pos
-							pos1.y = pos1.y+0.2
+							pos1.y = pos1.y + 0.2
 							local pos2 = object:getpos()
-							local vec = {x=pos1.x-pos2.x, y=pos1.y-pos2.y, z=pos1.z-pos2.z}
-							vec.x = vec.x*3
-							vec.y = vec.y*3
-							vec.z = vec.z*3
+							local vec = {x = pos1.x - pos2.x, y = pos1.y - pos2.y, z = pos1.z - pos2.z}
+							vec.x = vec.x * 3
+							vec.y = vec.y * 3
+							vec.z = vec.z * 3
 							object:setvelocity(vec)
 							object:get_luaentity().physical_state = false
 							object:get_luaentity().object:set_properties({
@@ -54,14 +54,13 @@ minetest.register_globalstep(function(dtime)
 									object:get_luaentity().itemstring = ""
 									object:remove()
 								else
-									object:setvelocity({x=0,y=0,z=0})
+									object:setvelocity({x = 0, y = 0, z = 0})
 									object:get_luaentity().physical_state = true
 									object:get_luaentity().object:set_properties({
 										physical = true
 									})
 								end
 							end, {player, object})
-							
 						end
 					end
 				end
@@ -75,7 +74,7 @@ function minetest.handle_node_drops(pos, drops, digger)
 	if minetest.setting_getbool("creative_mode") and digger and digger:is_player() then
 		inv = digger:get_inventory()
 	end
-	for _,item in ipairs(drops) do
+	for _, item in ipairs(drops) do
 		local count, name
 		if type(item) == "string" then
 			count = 1
@@ -85,19 +84,19 @@ function minetest.handle_node_drops(pos, drops, digger)
 			name = item:get_name()
 		end
 		if not inv or not inv:contains_item("main", ItemStack(name)) then
-			for i=1,count do
+			for i = 1, count do
 				local obj = minetest.add_item(pos, name)
 				if obj ~= nil then
 					obj:get_luaentity().collect = true
 					local x = math.random(1, 5)
-					if math.random(1,2) == 1 then
+					if math.random(1, 2) == 1 then
 						x = -x
 					end
 					local z = math.random(1, 5)
-					if math.random(1,2) == 1 then
+					if math.random(1, 2) == 1 then
 						z = -z
 					end
-					obj:setvelocity({x=1/x, y=obj:getvelocity().y, z=1/z})
+					obj:setvelocity({x = 1 / x, y = obj:getvelocity().y, z = 1 / z})
 					
 					-- FIXME this doesnt work for deactiveted objects
 					if minetest.setting_get("remove_items") and tonumber(minetest.setting_get("remove_items")) then
